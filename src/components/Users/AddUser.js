3baf58adeb7d2d1c6e63cca9ad62ad2d98ebc538
kqiu10 <qiukan97@gmail.com>
2021-07-23 22:00:12 -0400

    added re-usable button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-
+import Button from '../UI/Button'
+import Card from '../UI/Card'
+import classes from './AddUser.module.css'
 const AddUser = (props) => {
   const [name, setName] = useState('');
   const [year, setYear] = useState('');
@@ -17,18 +19,20 @@ const AddUser = (props) => {
 
   //addUser function
   const addUserHandler = (event) => {
-    evemt.preventDefault();
+    event.preventDefault();
     const newUser = [name, year]
     props.addUser(newUser)
   }
   return (
+    <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
       <label htmlFor='username'>Username</label>
       <input type='text' id='username'onChange={changeNameHandler}></input>
       <label htmlFor='age'>Age(Years)</label>
       <input id='age' type='number'onChange={changeAgeHandler}></input>
-      <button type='submit'>Add User</button>
-    </form>
+      <Button type='submit'>Add User</Button>
+      </form>
+      </Card>
   )
 }
 
